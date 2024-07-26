06d9dee8f6eabb0e5ff18a7cccdd64d71ee859ea
elikemdaniels <hellol@elikemdaniels.com>
2024-07-26 20:42:21 +0000

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="">
-      <h1>Hello world!</h1>
+      <h1>Hello NextJS</h1>
     </main>
   );
 }
